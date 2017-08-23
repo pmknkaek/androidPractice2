995b27bfc5c2dd9bf13435c9900513d32391b5c6
pmknkaek <cakesokorigins@gmail.com>
2017-08-22 21:55:54 -0600

    practiceClass

--- app/src/main/java/com/andy/androidpractice2/PracticeClass.java
+++ app/src/main/java/com/andy/androidpractice2/PracticeClass.java
@@ -4,9 +4,11 @@ package com.andy.androidpractice2;
  * Created by Andy on 8/22/2017.
  */
 
+
 public class PracticeClass {
     private String word;
     private String translation;
+
 
     public static final PracticeClass[] translations = {
             new PracticeClass("Hello", "Hallo"),
