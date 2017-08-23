aee316dde481e9625677854bc0f2a0632aa6aa1c
pmknkaek <cakesokorigins@gmail.com>
2017-08-22 21:14:42 -0600

    practiceClass

---
+++ app/src/main/java/com/andy/androidpractice2/PracticeClass.java
@@ -0,0 +1,32 @@
+package com.andy.androidpractice2;
+
+/**
+ * Created by Andy on 8/22/2017.
+ */
+
+public class PracticeClass {
+    private String word;
+    private String translation;
+
+    public static final PracticeClass[] translations = {
+            new PracticeClass("Hello", "Hallo"),
+            new PracticeClass("Skinny", "Schlank")
+    };
+
+    public PracticeClass(String word, String translation){
+        this.word = word;
+        this.translation = translation;
+    }
+
+    public String getWord(){
+        return word;
+    }
+
+    public String getTranslation(){
+        return translation;
+    }
+
+    public String toString(){
+        return word+" = "+translation;
+    }
+}
